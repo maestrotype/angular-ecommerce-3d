b7ab31474ce55c4202a90d4435084fe125d292bc
Andrii <andrey.danichkin@gentlecode.com>
2025-07-06 10:24:46 +0300

    fix orders config

--- backend/src/config/database.config.ts
+++ backend/src/config/database.config.ts
@@ -4,6 +4,7 @@ import { ConfigService } from '@nestjs/config';
 import { TypeOrmOptionsFactory, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { Product } from '../products/entities/product.entity';
 import { User } from '../auth/entities/user.entity';
+import { Order } from '../orders/entities/order.entity';
 
 @Injectable()
 export class DatabaseConfig implements TypeOrmOptionsFactory {
@@ -17,7 +18,7 @@ export class DatabaseConfig implements TypeOrmOptionsFactory {
       username: this.configService.get('DATABASE_USERNAME'),
       password: this.configService.get('DATABASE_PASSWORD'),
       database: this.configService.get('DATABASE_NAME'),
-      entities: [Product, User],
+      entities: [Product, Order, User],
       synchronize: this.configService.get('NODE_ENV') === 'development',
       logging: this.configService.get('NODE_ENV') === 'development',
       ssl: this.configService.get('NODE_ENV') === 'production' ? { rejectUnauthorized: false } : false,
