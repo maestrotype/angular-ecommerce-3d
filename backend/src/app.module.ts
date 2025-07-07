ad107b397291fb90917088536c48144b07487eed
Andrii <andrey.danichkin@gentlecode.com>
2025-07-07 16:44:13 +0300

    add category functionality

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsModule } from './products/products.module';
+import { CategoriesModule } from './categories/categories.module';
 import { AuthModule } from './auth/auth.module';
 import { OrdersModule } from './orders/orders.module';
 import { DatabaseConfig } from './config/database.config';
@@ -16,6 +17,7 @@ import { DatabaseConfig } from './config/database.config';
     TypeOrmModule.forRootAsync({
       useClass: DatabaseConfig,
     }),
+    CategoriesModule,
     ProductsModule,
     AuthModule,
     OrdersModule,
