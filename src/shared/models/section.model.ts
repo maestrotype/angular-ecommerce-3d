85c4501ec07e0e9b063f686687f5f9bee0e152db
Andrii <andrey.danichkin@gentlecode.com>
2025-07-10 17:26:32 +0300

    connection admin with front

---
+++ src/shared/models/section.model.ts
@@ -0,0 +1,10 @@
+interface Section {
+  id: number;
+  type: string; // 'hero', 'about', 'promo', ...
+  title: string;
+  content?: string;
+  imageUrl?: string;
+  order: number;
+  isActive: boolean;
+  settings?: any;
+}
