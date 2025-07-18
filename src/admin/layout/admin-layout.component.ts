e9e36f3708e4d94923a5ef807c152e9963864075
Andrii <andrey.danichkin@gentlecode.com>
2025-07-19 00:18:10 +0300

    fix translate

--- src/admin/layout/admin-layout.component.ts
+++ src/admin/layout/admin-layout.component.ts
@@ -1,9 +1,16 @@
 
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-admin-layout',
   templateUrl: 'admin-layout.component.html',
   styleUrls: ['admin-layout.component.scss'],
 })
-export class AdminLayoutComponent { }
+export class AdminLayoutComponent { 
+  constructor(private translate: TranslateService) {
+    const savedLang = localStorage.getItem('adminLang') || 'en';
+    this.translate.setDefaultLang(savedLang);
+    this.translate.use(savedLang);
+  }
+}
