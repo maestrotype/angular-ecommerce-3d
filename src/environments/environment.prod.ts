2d63a9f4deba2b70df293431505fe18a7f7e2fdf
Andrii <adan3891@gmail.com>
2025-09-05 22:01:51 +0300

    broken changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://angular-ecommerce-backend.onrender.com/api',
+    apiUrl: 'http://localhost:3002/api',
+    // apiUrl: 'https://angular-ecommerce-backend.onrender.com/api',
     stripePublishableKey: 'pk_test_mock_key_for_testing_only',
     paypalClientId: 'mock_paypal_client_id_for_testing'
   };
